76082b7499b6318efacbcd588746a086811850ea
Dianda Kacinta <dianda.kacinta@gmail.com>
2014-05-21 16:26:27 +0700

    Commit 1 : @Before

---
+++ ujianPraktikumRKPPL113040249/src/org/ifunpas/rkppl/TestingRKPPL.java
@@ -0,0 +1,16 @@
+package org.ifunpas.rkppl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestingRKPPL {
+
+	Soal soal;
+	
+	@Before
+	public void mulaiTest(){
+		System.out.println("Mulai Test");
+		soal = new Soal();
+	}
+
+}
